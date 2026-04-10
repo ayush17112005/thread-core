63eae42937a0123658609e42b4cee529d14fa720
ayush17112005 <saxenaayushman99@gmail.com>
2026-04-10 15:08:30 +0530

    feat: Add MongoDB setup

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 import express from "express";
+import { connectDB } from "./src/configs/db.js";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
 
-const port = process.env.PORT || 4000;
-
+app.use(express.json());
+const port = process.env.PORT || 3000;
+await connectDB();
 app.get("/", (req, res) => {
   res.send("Health check successful!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
