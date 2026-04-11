5a00cb980b63e21afec64556b5651295d824cf4b
ayush17112005 <saxenaayushman99@gmail.com>
2026-04-11 21:45:36 +0530

    Complete Login Flow with JWT Generation

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,8 @@
 import { User } from "../models/userSchema.js";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 const registerUserService = async (username, email, password) => {
   //Check if user already exists
   const existingUser = await User.findOne({ $or: [{ username }, { email }] });
@@ -12,4 +15,32 @@ const registerUserService = async (username, email, password) => {
   return newUser;
 };
 
-export { registerUserService };
+const loginUserService = async (email, password) => {
+  //see if this email is in db or not
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new Error("Invalid email or password");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Invalid email or password");
+  }
+
+  //IF the credentials are correct then give him token and let him in
+  const token = jwt.sign(
+    {
+      sub: user._id,
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }, //because it's a access token it's lifespan is short, we will create refresh token in future which will have longer lifespan
+  );
+  return {
+    user: {
+      id: user._id,
+      email: user.email,
+      name: user.username,
+    },
+    token,
+  };
+};
+export { registerUserService, loginUserService };
