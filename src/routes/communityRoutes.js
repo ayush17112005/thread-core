800877d01622b9cc6da0a3cb927bf16aa2571cd0
ayush17112005 <saxenaayushman99@gmail.com>
2026-04-13 00:30:58 +0530

    feat: Implement joinCommunityRotue, joinCommunityService, joinCommuniytController

--- src/routes/communityRoutes.js
+++ src/routes/communityRoutes.js
@@ -1,10 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middlewares/authMiddleware.js";
-import { createCommunityController } from "../controllers/communityController.js";
+import {
+  createCommunityController,
+  joinCommunityController,
+} from "../controllers/communityController.js";
 
 const router = express.Router();
 
 //Create community route
 router.post("/", protectRoute, createCommunityController);
 
+//Join Community route
+router.post("/:communityId/join", protectRoute, joinCommunityController);
 export default router;
