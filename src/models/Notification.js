eda69e977e86640e8b34b8742ae4cb1eed2cd437
Ayushman Saxena <ayush@Ayusman-HP.localdomain>
2026-08-19 11:29:49 +0530

    Add Notification Schema

---
+++ src/models/Notification.js
@@ -0,0 +1,56 @@
+import { Schema, model } from "mongoose";
+
+const notificationSchema = new Schema(
+  {
+    recipient: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    actor: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    action: {
+      type: String,
+      required: true,
+      enum: [
+        "like",
+        "comment",
+        "comment_reply",
+        "community_join",
+        "community_kick",
+      ],
+    },
+
+    entityId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      refPath: "entityModel",
+    },
+
+    entityModel: {
+      type: String,
+      required: true,
+      enum: ["Post", "Comment", "Community"],
+    },
+
+    isRead: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+notificationSchema.index({
+  recipient: 1,
+  createdAt: -1,
+});
+
+export const Notification = model("Notification", notificationSchema);
